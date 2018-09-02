6f0838082b98ae3be293676d38f271c689f39c49
AntonElenberger <antonelenberger@gmail.com>
2018-09-02 22:20:51 +0500

    Home Work for Lesson 6
    First two tasks.

---
+++ src/test/java/TestOfTask1.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+import ru.geekbrains.anronelenberger.homework6.*;
+
+public class TestOfTask1 {
+
+    @Test
+    public void test1() {
+        int[] array1 = {5, 6, 7};
+        Assert.assertArrayEquals(MainClass.task1(1, 2, 3, 4, 5, 6, 7), array1);
+    }
+
+    @Test
+    public void test2() {
+        int[] array2 = {7};
+        Assert.assertArrayEquals(MainClass.task1(1, 4, 3, 4, 5, 6, 7), array2);
+    }
+
+    @Test
+    public void test3() {
+        int[] array3 = {};
+        Assert.assertArrayEquals(MainClass.task1(1, 2, 3, 4, 5, 6, 4), array3);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test4() {
+        MainClass.task1(1, 2, 3, 5, 6, 7, 8, 0);
+    }
+}
